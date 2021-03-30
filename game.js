5eb2f6a7c6bc62351c3a68393fb14b481e16bb6a
anirudhlohi <74246267+anirudhlohi@users.noreply.github.com>
2021-03-30 18:53:49 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -29,18 +29,22 @@ row = question + input_box + button_check
 document.getElementById("output").innerHTML = row
 }
 function check(){
+    question_turn = "player1"
+    answer_turn = "player2"
     get_answer = document.getElementById("get_a").value;
     document.getElementById("output").innerHTML = ""
     if (get_answer == actual_ans){
-        player1_score = player1_score + 1
-        document.getElementById("player1_score").innerHTML = player1_score
+        if(answer_turn == "player1"){
+            player1_score = player1_score + 1
+            document.getElementById("player1_score").innerHTML = player1_score   
+        }
+        else{
+            player2_score = player2_score + 1
+            document.getElementById("player2_score").innerHTML = player2_score
+        }
     }
-    else{
-        player2_score = player2_score + 1
-        document.getElementById("player2_score").innerHTML = player2_score
-    }
-    question_turn = "player1"
-    answer_turn = "player2"
+    
+    
     if(question_turn == "player1"){
         question_turn = "player2"
         answer_turn = "player1"
